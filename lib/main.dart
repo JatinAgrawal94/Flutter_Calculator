48d48e4510ac9386f7722e57c1cbcc4985c11aa9
JatinAgrawal94 <jatinagrawal0801@gmail.com>
2020-08-07 21:47:20 +0530

    final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'dart:math';
 
 void main() {
   runApp(MyApp());
@@ -9,136 +9,121 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title:'Flutter Demo',
+      theme:ThemeData(
+        primarySwatch: Colors.blue
       ),
-      home: MyHomePage(),
+      home:MyHomePage(title:'Calculator '),
     );
+
   }
 }
 
+
 class MyHomePage extends StatefulWidget {
+
+  MyHomePage({Key key,this.title}):super(key: key);
+
+  final String title;
   @override
   _MyHomePageState createState() => _MyHomePageState();
+
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+    String output="0";
+
+    String _output="0";
+    double num1=0.0;
+    double num2=0.0;
+    String operand="";
+    buttonPressed(String buttonText){
+      if(buttonText=="CLEAR"){
+        String _output="0";
+        double num1=0.0;
+        double num2=0.0;
+        String operand="";
+      }
+      else if(buttonText == '+' || buttonText == '-' || buttonText == 'x' || buttonText == '/'){
+        num1=double.parse(output);
+        operand=buttonText;
+        _output="0";
+      }
+      else if(buttonText=='.'){
+          if(_output.contains('.')){
+            print("Already contains a decimal");
+            return;
+          }
+          _output=_output+buttonText;
+      }
+      else if(buttonText=='='){
+        num2=double.parse(output);
+
+      }
+    }
+
+
+  // button function
+    Widget buidButton( String buttonText){
+        return Expanded(
+          child: OutlineButton(
+            padding:EdgeInsets.all(24.0),
+            child: Text(buttonText,style:TextStyle(fontSize:20.0,fontWeight:FontWeight.bold)),
+            onPressed: (){},
+          ),
+        );
+    }
   @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: 3,
-        child: Scaffold(
-            appBar:PreferredSize(preferredSize: Size.fromHeight(70.0),
-            child:AppBar(
-              flexibleSpace:new Container(
-                padding: EdgeInsets.only(top:20.0),
-                child:TabBar(
-                  tabs: <Widget>[
-                    Tab(text: 'Page1', icon: Icon(Icons.drag_handle)),
-                    Tab(
-                      text: 'Page2',
-                      icon: Icon(Icons.view_compact),
-                    ),
-                    Tab(text: 'he',icon:Icon(Icons.view_compact))
-                  ],
-                )
-              ,)
-            ),),
+    Widget build(BuildContext context){
+      return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title)
+        ),
+        body: new Container(
 
-            body: TabBarView(
-              children: <Widget>[
-                PageOne(),
-                PageTwo(),
-                PageOne()
-              ],
-            )));
-  }
-}
-
-class PageOne extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-      Align(
-            alignment: Alignment.topLeft,    //alignment for the container
-            child:Container(
-  //          padding: EdgeInsets.only(top:20.0),
-  //          margin: EdgeInsets.only(top:150),
-  //            color:Colors.red,
-            height:MediaQuery.of(context).size.height*0.35,
-            width:MediaQuery.of(context).size.width,
-              child:Align(                  //alignment for the textfield in container
-                alignment: Alignment.bottomLeft,
-                child: TextField(
-                    textAlign: TextAlign.right,
-                    style: TextStyle(
-                      fontSize: 55,
-                      backgroundColor:Colors.black,
-                      ),
-                    decoration: InputDecoration(
-                      border:OutlineInputBorder(
-                        borderSide:BorderSide(
-                        )
-                      ),
-                  ),
-          )
-              ),
-
-          )
-      ),
-        Container(
-          child:GridView.count(
-            primary: false,
-            padding: const EdgeInsets.all(20),
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            crossAxisCount: 2,
-            
+          child: Column(
             children: <Widget>[
               Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text("He'd have you all unravel at the"),
-                color: Colors.teal[100],
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text('Heed not the rabble'),
-                color: Colors.teal[200],
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text('Sound of screams but the'),
-                color: Colors.teal[300],
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text('Who scream'),
-                color: Colors.teal[400],
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text('Revolution is coming...'),
-                color: Colors.teal[500],
-              ),
-              Container(
-                padding: const EdgeInsets.all(8),
-                child: const Text('Revolution, they...'),
-                color: Colors.teal[600],
-              ),
+                alignment:Alignment.centerRight,
+                padding:EdgeInsets.symmetric(vertical: 24.0,horizontal:12.0),
+                  child: Text(output,style: TextStyle(fontSize: 48.0,fontWeight:FontWeight.bold))),
+              Expanded(child: Divider()),
+              Column(children: <Widget>[
+
+                Row(children: <Widget>[
+                  buidButton('7'),
+                  buidButton('8'),
+                  buidButton('9'),
+                  buidButton('/')
+                ],),
+                Row(children: <Widget>[
+                  buidButton('4'),
+                  buidButton('5'),
+                  buidButton('6'),
+                  buidButton('X')
+                ],),
+                Row(children: <Widget>[
+                  buidButton('1'),
+                  buidButton('2'),
+                  buidButton('3'),
+                  buidButton('-')
+                ],),
+                Row(children: <Widget>[
+                  buidButton('.'),
+                  buidButton('0'),
+                  buidButton('00'),
+                  buidButton('+')
+                ],),
+                Row(children: <Widget>[
+                  buidButton("CLEAR"),
+                  buidButton('='),
+                ],)
+              ],)
             ],
-          )
+          ),
         )
-      ]
-    );
-  }
-}
+      );
+    }
 
-class PageTwo extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(child: Center(child: Text('This is page two')));
-  }
 }
-
